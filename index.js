df89254b61730652d832c38d5833a61173ebf940
Brynfor Morris <brynfor.morris@gmail.com>
2023-01-09 18:15:29 -0600

    commit tree 5695475f5b8bb969198026de15d528b80d164541

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+    function destructivelyAppendCat(name){
+        const newFunction = cats.push(name);
+    };
+
+    function destructivelyPrependCat(name){
+        const newFunction2 = cats.unshift(name);
+    };
+
+    function destructivelyRemoveLastCat(){
+        const newFunction3 = cats.pop();
+    };
+
+    function destructivelyRemoveFirstCat(){
+        const newFunction4 = cats.shift();
+    };
+
+    function appendCat(name){
+        const copyCats = [...cats.slice(),name];
+        return copyCats;
+    };
+
+    function prependCat(name){
+        const copyCatts = [name,...cats.slice()];
+        return copyCatts
+    }
+
+    function removeLastCat(){
+        const removekitty = [...cats.slice(0,2)]
+        return removekitty;
+    };
+
+    function removeFirstCat(){
+        const removeFirstKitty = [...cats.slice(1)]
+        return removeFirstKitty;
+    }
